a40441e6c5bd1a942310266ccc7423ed0b7c17a1
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-04-30 22:15:06 +0600

    Fucntion with Return_type and Parameter

---
+++ function_with_Return+No_Parameter.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int sum
+
+int main()
+{
+    
+    
+    return 0;
+}
--- function_with_Return+Parameter.c
+++ function_with_Return+Parameter.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int sum(int x,int y)
+int sum(int x, int y)
 {
-    int sum = x+y;
+    int sum = x + y;
 
-    return sum;//eitar data type e hobe function er data type/return type
+    return sum; // eitar data type e hobe function er data type/return type
 }
-//int sum(int x,int y); declaration of a function, it needs when we make a function below the main function
+// int sum(int x,int y); declaration of a function, it needs when we make a function below the main function
 
 int main()
 {
@@ -14,13 +14,15 @@ int main()
     // int ans = sum(10,20);
 
     // printf("SUM = %d\n",ans);
+    // int a,b;
+    int x, y;
+    scanf("%d %d", &x, &y);
 
-    printf("SUM = %d\n",sum(10,20));
-    printf("SUM = %d\n",sum(20,30));
-    
+    printf("SUM = %d\n", sum(x, y));
+    printf("SUM = %d\n", sum(20, 30));
+
     return 0;
 }
-
 
 // int sum(int x,int y)
 // {
